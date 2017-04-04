f32a041a1dceaf247d6e3e8b56b53171c3f17ca8
Kelly F Z <kzimmerman@energy-hub.net>
2017-04-03 20:29:11 -0400

    original commit of echo

---
+++ src/PrintMyName.java
@@ -0,0 +1,10 @@
+
+public class PrintMyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String NandAge = "Jesse Jang: Age 22";
+System.out.println(NandAge);
+	}
+
+}
